a111319160095c49a05b98e00a187b1a0089b37c
Barac Sebastian-Beniamin <barac.sebastian.beniamin@gmail.com>
2024-08-26 23:59:00 +0300

    Add: modal pop-up and styling for add photo

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 <li>style the main elements</li>
 <li>add grid or flexbox to gallery</li>
 <li>make it responsive</li>      
-<li>create interactive code</li>      
+<li>create interactive code</li>
+<li>add photo functionality</li>      
+<li>add filter tags functionality</li>      
+<li>store photos locally or in a database</li>
+<li>display photos on different pages</li>
 <li>filter images</li>      
 <li>add lightbox functionality</li>      
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //selectors
 const darkModeButton = document.getElementById("dark-mode-button");
+const addPhotoButton = document.getElementById("add-photo-button");
+const addPhotoModal = document.getElementById("add-photo-modal");
 
 //state
 const theme = localStorage.getItem('theme');
@@ -17,5 +19,17 @@ handleThemeToggle = () => {
     }
 };
 
+handleAddPhotoModalePositioning = () => {
+    addPhotoModal.classList.remove('add-photo-modal-hidden');
+    
+    const addPhotoButtonRect = addPhotoButton.getBoundingClientRect();
+    const addPhotoModalRect = addPhotoModal.getBoundingClientRect();
+
+    addPhotoModal.style.bottom = `${window.innerHeight - addPhotoButtonRect.top + 20}px`;
+    addPhotoModal.style.left = `${addPhotoButtonRect.left + (addPhotoButtonRect.width / 2) - (addPhotoModalRect.width / 2)}px`;
+    console.log(addPhotoModalRect );
+};
+
 //events
 darkModeButton.addEventListener('click', handleThemeToggle);
+addPhotoButton.addEventListener('click', handleAddPhotoModalePositioning)
